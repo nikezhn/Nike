4d53071628999bd8a7a167a4cde8af5d40254fea
nikezhn <1761216556@qq.com>
2019-09-21 16:59:03 +0800

    新增了控制器

---
+++ consumer-8080/src/main/java/com/cssl/controller/UsersController.java
@@ -0,0 +1,15 @@
+package com.cssl.controller;
+
+import com.cssl.pojo.Nike_color;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UsersController {
+
+    Nike_color nike_color;
+
+    public void show(){
+
+    }
+
+}
