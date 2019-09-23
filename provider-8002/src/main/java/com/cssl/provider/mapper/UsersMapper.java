0e6527e3c95c8827491539d89421d1659c4a5e74
Nikekdb <1986588053@qq.com>
2019-09-23 16:15:19 +0800

    查询

--- provider-8002/src/main/java/com/cssl/provider/mapper/UsersMapper.java
+++ provider-8002/src/main/java/com/cssl/provider/mapper/UsersMapper.java
@@ -1,8 +1,13 @@
 package com.cssl.provider.mapper;
 
+import com.cssl.pojo.Nike_product;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface UsersMapper {
+
+    List<Nike_product> listSel();//查询所有
 
 }
