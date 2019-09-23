3895444a09c46e3407a74d8ff06ea4e859eacd96
Nikekdb <1986588053@qq.com>
2019-09-23 16:17:16 +0800

    0.0

--- provider-8002/src/main/java/com/cssl/provider/mapper/UsersMapper.java
+++ provider-8002/src/main/java/com/cssl/provider/mapper/UsersMapper.java
@@ -1,6 +1,7 @@
 package com.cssl.provider.mapper;
 
 import com.cssl.pojo.Nike_product;
+import com.cssl.pojo.Nike_users;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -8,6 +9,8 @@ import java.util.List;
 @Mapper
 public interface UsersMapper {
 
-    List<Nike_product> listSel();//查询所有
+    List<Nike_product> listSel();//查询所有;
+
+    Nike_users log();//登陆;
 
 }
