700dac10d7ed1e45a874fe6d199db72f5b8221fe
Nikekdb <1986588053@qq.com>
2019-09-24 16:35:39 +0800

    实验成功!!!

---
+++ consumer-8081/src/main/java/com/cssl/controller/UsersController.java
@@ -0,0 +1,30 @@
+package com.cssl.controller;
+
+import com.cssl.pojo.Nike_color;
+import com.cssl.service.INike_colorClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+public class UsersController {
+
+    @Autowired
+    INike_colorClientService iNike_colorClientService;
+
+    @RequestMapping("/findAll")
+    String findAll(){
+        List<Nike_color> list=iNike_colorClientService.findAll();
+        System.out.println(list);
+        return "index";
+    }
+
+    @RequestMapping("user")
+    String user(){
+        return "user";
+    }
+
+}
